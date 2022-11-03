e7bde5930951d2ed8bc5df5dcd0a778910bf1ea4
EliotRagueneau <eragueneau@ebi.ac.uk>
2022-11-03 14:55:01 +0000

    :sparkles: Add folder indexing script

---
+++ scripts/map-folder.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+if ["$#" -ne 3]; then
+    echo "USAGE ./map-folder.sh INPUT_FOLDER MAPPING_FILE OUTPUT_FOLDER"
+    echo ""
+    echo "INPUT_FOLDER can contain the prefix of the flat files"
+    echo "MAPPING_FILE is an XML file created by XMLMaker after saving a mapping"
+    echo "OUTPUT_FOLDER is the path to which the generated xml should be put in"
+    exit 1;
+fi
+
+INPUT_FOLDER=$1
+MAPPING_FILE=$2
+OUTPUT_FOLDER=$3
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+for FILE in ${INPUT_FOLDER}*;
+do
+    FILENAME=$(basename -- "$FILE")
+    ${SCRIPT_DIR}/../target/xmlMakerFlattener/bin/xmlmaker \
+        -flatfiles ${FILE} \
+        -mapping ${MAPPING_FILE} \
+        -o "${OUTPUT_FOLDER}/$(echo "${FILENAME}" | sed 's/[tc]sv/xml/g')"
+done;
